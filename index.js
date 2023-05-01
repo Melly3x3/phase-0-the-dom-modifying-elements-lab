360b43070872e30d153b2059b8e7a70bbb17a640
Melvin <jmelvin1134@gmail.com>
2023-04-30 21:12:57 -0400

    dom lab complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.querySelector('main');
+main.remove();
+main.id = 'victory'
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+newHeader.textContent = 'Melvin is the champion'
+console.log(newHeader);
